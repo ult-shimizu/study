ce892acda154ea9a0d7385ee82d3eb7da15b4b71
shimizu <s.shimizu@ultinet.co.jp>
2020-03-03 17:36:41 +0900

    ansible・.envファイルの設定等 ・.envファイルの作成をansibleで行う（変数で定義することで環境で設定値が可変になる） ・Ansible-Vaultを用いてDBのpass等を管理する ・TODO：EC２を作成後にIPを定義する ・TODO：deployerの設定

--- deploy.php
+++ deploy.php
@@ -4,13 +4,13 @@ namespace Deployer;
 require 'recipe/laravel.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'study');
 
 // Project repository
-set('repository', 'git@domain.com:username/repository.git');
+set('repository', 'git@https://github.com/ult-shimizu/study.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', false);
 
 // Shared files/dirs between deploys 
 add('shared_files', []);
@@ -22,17 +22,33 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('project.com')
-    ->set('deploy_path', '~/{{application}}');    
+inventory('hosts.yml');
     
 // Tasks
 
-task('build', function () {
-    run('cd {{release_path}} && build');
-});
+task('deploy:laravel', [
+        'deploy:info',
+        'deploy:prepare',
+        'deploy:lock',
+        'deploy:release',
+        'deploy:update_code',
+        'deploy:shared',
+        'deploy:vendors',
+        'deploy:writable',
+        'artisan:storage:link',
+        'artisan:view:clear',
+        'artisan:cache:clear',
+        'artisan:config:cache',
+        'artisan:optimize',
+        'deploy:symlink',
+        'deploy:unlock',
+        'cleanup',
+    ]
+);
 
 // [Optional] if deploy fails automatically unlock.
-after('deploy:failed', 'deploy:unlock');
+after('deploy:failed', 'deploy:
+unlock');
 
 // Migrate database before symlink new release.
 
